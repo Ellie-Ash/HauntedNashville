f8f9e3823be1b9b66078433ff1dbb44e67ec1436
Ellie Ash <ellieash4000@gmail.com>
2019-09-25 11:40:08 -0500

    saved location header

--- src/components/saved/SavedLocationMain.js
+++ src/components/saved/SavedLocationMain.js
@@ -5,6 +5,7 @@ class SavedLocationMain extends Component {
     render() {
         return (
             <React.Fragment>
+                <h4 className="savedLocationHeader">Saved Locations</h4>
                 <SavedLocationList />
             </React.Fragment>
         )
