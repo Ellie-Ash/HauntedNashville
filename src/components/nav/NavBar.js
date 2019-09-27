70f339e8d1a3afd32482a0d45864db4a56b91b12
Ellie Ash <ellieash4000@gmail.com>
2019-09-26 19:16:32 -0500

    changed header

--- src/components/nav/NavBar.js
+++ src/components/nav/NavBar.js
@@ -28,7 +28,7 @@ class NavBar extends Component {
                         <Link onClick={this.logout} className="navLink" to="/"><span style={{ color: 'whitesmoke' }}><i className="fas fa-sign-out-alt"></i></span></Link>
                     </li>
                 </ul> }
-            <h1 className="mainHeader"> HAUNTED NASHVILLE </h1>
+            <h1 className="mainHeader"> Haunted Nashville </h1>
             {/* <div className="divider"><img src="https://s-media-cache-ak0.pinimg.com/originals/d8/8d/b2/d88db24705469bee3122183d38192a4e.png" alt="..." className="dividerImg"></img> 
             </div> */}
             
